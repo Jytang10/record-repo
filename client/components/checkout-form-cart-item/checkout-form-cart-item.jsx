3f63a96eb2b9854173f83923cd81d7733c107350
James Tang <jytang10@gmail.com>
2019-07-07 07:08:41 +0000

    Completed modals, page finetuning

--- client/components/checkout-form-cart-item/checkout-form-cart-item.jsx
+++ client/components/checkout-form-cart-item/checkout-form-cart-item.jsx
@@ -4,23 +4,15 @@ import './checkout-form-cart-item.css';
 class CheckoutFormCartItem extends React.Component {
   render() {
     const productImage = this.props.item.image;
-    // const cardStyle = { 'width': '18rem' };
+    let convertItemPrice = this.props.item.price / 100;
+    let fixedItemPrice = convertItemPrice.toFixed(2);
+    let itemSubtotal = this.props.item.quantity * fixedItemPrice;
     return (
-      // <Card className="shadow p-2 mb-5 bg-white rounded">
-      //   <CardImg className="product-image" top width="100%" src={productImage} alt="Item image" />
-      //   <CardBody>
-      //     <CardTitle className="product-title">{this.props.item.name}</CardTitle>
-      //     <CardSubtitle className="product-artist">{this.props.item.artist}</CardSubtitle>
-      //     <CardText className="product-price">{'$' + (this.props.item.price / 100)}</CardText>
-      //     <CardText className="product-text">{this.props.item.shortDescription}</CardText>
-      //   </CardBody>
-      // </Card>
-      // <tbody>
       <tr>
-        <td><img className="cart-image" width="100%" src={productImage} alt="Cart item image" /></td>
+        <td><img className="cart-image" width="100rem" src={productImage} alt="Cart item image" /></td>
         <td>{this.props.item.name}</td>
-        <td>Quantity</td>
-        <td>Subtotal</td>
+        <td>{this.props.item.quantity}</td>
+        <td>${itemSubtotal}</td>
       </tr>
     );
   }
