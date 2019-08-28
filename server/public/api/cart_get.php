a185f4d58797901e01dfad384eb2ede81ec0ef56
James Tang <jytang10@gmail.com>
2019-08-28 07:23:24 +0000

    Fixed cartId naming convention and updated cart_get

--- server/public/api/cart_get.php
+++ server/public/api/cart_get.php
@@ -5,38 +5,45 @@
 		exit();
 	}
 
-  if(empty($_SESSION['cart_id'])){        // Check if SESSION[‘cart_id’] is empty
+  if(empty($_SESSION['cartId'])){        // Check if SESSION[‘cart_id’] is empty
     print(json_encode(array()));          // If it is, print a json encoded empty array
     exit();                               // Exit to stop processing, we have no cart for this person
   }
 
-  $cartId = intval($_SESSION['cart_id']);  // Set the $cartId variable to the SESSION cart_id. To be safe, we probably should intval it, too
+  $cartId = intval($_SESSION['cartId']);  // Set the $cartId variable to the SESSION cart_id. To be safe, we probably should intval it, too
                                           // Write a query that fetches the appropriate data as found in dummy-cart-items.json
                                           // You’ll need a join with products table to get data from there
                                           // You’ll need a subquery to get the first image from images table
                                           // You’ll need to only get the cart where the cart ID is the one you are looking for
   $cart_query = "SELECT 
-    c.`created`,
-    ci.`count`, ci.`price`
-    p.`id`, p.`name`, p.`price`,
-    (SELECT url FROM `images` WHERE `products_id` = p.`id` LIMIT 1 ) AS `image`
+      c.`created`,
+      ci.`count`, ci.`price`
+      p.`id`, p.`name`, p.`price`, p,`artist`,
+      (SELECT `url` FROM `images` WHERE `products_id` = p.`id` LIMIT 1) AS url
     FROM `cart` AS `c` 
-    JOIN `cartItems` AS `ci` ON ci.`cartId` = c.`id`
-    JOIN `products` AS `p` ON ci.`productId` = p.`id`
-    WHERE c.`id` = $cart_id
+    JOIN `cartItems` AS `ci` ON ci.`cartID` = c.`id`  
+    JOIN `products` AS `p` ON ci.`productID` = p.`id`
+    WHERE c.`id` = {$cartId}
     ";
   
-  $cart_data = mysqli_query($conn, $cart_query);  // Send the query to mysql and get the result
+  $cart_result = mysqli_query($conn, $cart_query);  // Send the query to mysql and get the result
 
-  if(!$cart_data){
+  if(!$cart_result){
     throw new Exception(mysqli_error($conn));
   }
 
-  if(mysqli_num_rows($cart_data) === 0){  // If there is nothing there, make sure it prints out an empty array
-    print(json_encode(array()));
-    exit(); 
+  $data = [];
+  while($row = mysqli_fetch_assoc($cart_result)) {  // Iterate through array until data runs out
+    $data[] = $row;                                 // while tests a falsey value which stops the loop
   }
 
-  print(json_encode($cart_data));  //Retrieve the data you got from the query and print it out.
+  if($data === []) {                // If query id does not exist, no result is returned (test for valid id). Print out an empty array
+    print(json_encode(array()));
+  }
 
+  if(count($data) === 1) {        //Retrieve the data you got from the query and print it out.
+    print(json_encode($data));    // index 0 because this returns an array with one object inside
+  } else {
+    print(json_encode($data));
+  }
 ?>
