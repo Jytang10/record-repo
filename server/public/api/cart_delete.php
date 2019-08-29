4f7432a564eae5fe593b6dd08429e47384b28786
James Tang <jytang10@gmail.com>
2019-08-29 04:09:46 +0000

    Added internal, id and session cartid checks

--- server/public/api/cart_delete.php
+++ server/public/api/cart_delete.php
@@ -1,4 +1,26 @@
 <?php
 
+  if (!defined('INTERNAL')) {
+    print('Cannot allow direct access'); 
+    exit();
+  }
+
+  $data = getBodyData();
+
+  if(isset($data['id'])) {
+		$id = intval($data['id']);
+		if ($id < 1) {
+			throw new Exception('invalid id. must be a number greater than 0');
+		}
+	} else {
+		throw new Exception('Must have a product id to add to cart');
+  }
+
+  if (empty($_SESSION['cartId'])) {
+    print(json_encode(array()));
+    exit();
+	} else {
+		$cartId = $_SESSION['cartId'];	
+  }
 
 ?>
